9b2f8d50bd2321990819ef082b6e3edfbf468613
Howard Lo <he01350446@gmail.com>
2016-12-09 23:58:35 +0800

    Update linggle temporal API url

--- server/api/linggle.py
+++ server/api/linggle.py
@@ -4,10 +4,10 @@ import urllib
 def parseData(rowdict):
     def extract_word(word):
         return word.replace('<strong>', '').replace('</strong>', '').strip()
-    return (' '.join(map(extract_word, rowdict['phrase'])), float(rowdict['count']))
+    return (' '.join(map(extract_word, rowdict['phrase'])), float(rowdict['count_str']))
 
 def linggleit(query):
-    url = 'http://linggle.com/query/{}'.format(urllib.quote(query, safe=''))
+    url = 'http://lost.nlpweb.org:9487/?search={}'.format(urllib.quote(query, safe=''))
     r = requests.get(url)
     if r.status_code == 200:
         res = dict()
